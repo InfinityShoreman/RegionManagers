f76df91e0a2d3abd4d83249044714214428b144c
SirSavary <jesse.savary@live.com>
2012-05-22 19:19:37 -0700

    Added /town map

---
+++ src/me/sirsavary/townmanager/TownMapRenderer.java
@@ -0,0 +1,129 @@
+package me.sirsavary.townmanager;
+
+import java.util.ArrayList;
+
+import me.sirsavary.townmanager.objects.Town;
+import me.sirsavary.townmanager.objects.TownChunk;
+
+import org.bukkit.Chunk;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.map.MapCanvas;
+import org.bukkit.map.MapCursorCollection;
+import org.bukkit.map.MapPalette;
+import org.bukkit.map.MapRenderer;
+import org.bukkit.map.MapView;
+import org.bukkit.map.MinecraftFont;
+
+public class TownMapRenderer extends MapRenderer {
+
+
+	private int renderIndex = 0;
+	private int MAP_SECOND_DELAY = 5;
+
+	private MapCanvas canvas;
+
+	@Override
+	public void render(MapView map, MapCanvas canvas, Player p) {
+
+		Byte lastWildernessColor = MapPalette.BROWN;
+		
+		renderIndex++;
+		if (((renderIndex % MAP_SECOND_DELAY) * 20) != 0) return;
+		renderIndex = 0;
+
+		//if (p.getItemInHand().getType() != Material.MAP)
+			//return;
+
+		this.canvas = canvas;
+
+		TownChunk[][] chunkArray = new TownChunk[7][5];
+		Chunk playerChunk = p.getLocation().getChunk();
+
+		int playerX = playerChunk.getX();
+		int playerZ = playerChunk.getZ();
+
+		ArrayList<TownChunk> chunks =  Main.fileManager.getChunks();
+		if (chunks != null) {
+			for (TownChunk tc : chunks)
+				if ((tc.getX() <= (playerX + 3)) && (tc.getX() >= (playerX - 3)) && (tc.getZ() <= (playerZ + 2)) && (tc.getZ() >= (playerZ - 2)))
+					chunkArray[(playerX - tc.getX()) + 3][(playerZ - tc.getZ()) + 2] = tc;
+		}
+		
+		canvas.drawText(1, 9, MinecraftFont.Font, "Wilderness is gray");
+		canvas.drawText(1, 19, MinecraftFont.Font, "Your town is green");
+		canvas.drawText(1, 29, MinecraftFont.Font, "Other towns are blue");
+
+		for (int x = 0; x < 7; x++) {
+			for (int y = 0; y < 5; y++) {
+					
+				Byte color = null;
+				TownChunk tc = chunkArray[x][y];
+				Town t = null;
+
+				if (tc == null) //No townchunk, aka wilderness
+				{
+					if (lastWildernessColor == MapPalette.DARK_GRAY) {
+						color = MapPalette.GRAY_1; 
+						lastWildernessColor = MapPalette.GRAY_1;
+					}
+					else {
+						color = MapPalette.DARK_GRAY; 
+						lastWildernessColor = MapPalette.DARK_GRAY;
+					}
+				}
+				else {
+					Town playerTown = Main.fileManager.getPlayerTown(p);
+					t = Main.fileManager.getTown(tc.getTown());
+					if ((playerTown != null) && (t == playerTown)) color = MapPalette.LIGHT_GREEN;//TownChunk belongs to players town
+					else color = MapPalette.BLUE; //Other Town
+					if (lastWildernessColor == MapPalette.DARK_GRAY) {
+						lastWildernessColor = MapPalette.GRAY_1;
+					}
+					else {
+						lastWildernessColor = MapPalette.DARK_GRAY;
+					}
+				}
+
+				setCanvasSquare(color, (x * 18) + 1, 109 - y * 18, 18);
+				MapCursorCollection cursors = canvas.getCursors();
+				for (int i = 0; i < cursors.size(); i++) {
+					cursors.removeCursor(cursors.getCursor(i));
+				}
+				
+				//MapCursor mc = new MapCursor((byte) 0, (byte) 36, (byte) 15, (byte) 0, true);
+				//cursors.addCursor(mc);
+			}
+		}
+	}
+
+	private void setCanvasSquare(byte color , int x , int z , int size){
+		for(int t = x; t < (x + size); t++)
+			for(int ta = z; ta < (z + size); ta++)
+					canvas.setPixel(t , ta, color);
+	}
+	
+	public static void CreateMap(Player player) {
+		ItemStack is = new ItemStack(Material.MAP);
+		
+		int mapNumber;
+		try {
+			mapNumber = Main.fileManager.getPlayerMap(player.getName());
+		} catch (NullPointerException e) {
+			mapNumber = (int) Main.server.createMap(player.getWorld()).getId();
+			Main.fileManager.setPlayerMap(player.getName(), mapNumber);
+		}
+		
+		is.setDurability((short) mapNumber);
+		player.setItemInHand(is);
+		
+		TownMapRenderer.drawToMap(Main.server.getMap((short) mapNumber));
+	}
+
+	public static void drawToMap(MapView map){
+		for (MapRenderer renderer : map.getRenderers())
+			map.removeRenderer(renderer);
+		map.addRenderer(new TownMapRenderer());
+	}
+}
--- src/me/sirsavary/townmanager/commands/TownCommand.java
+++ src/me/sirsavary/townmanager/commands/TownCommand.java
@@ -2,6 +2,7 @@ package me.sirsavary.townmanager.commands;
 
 import me.sirsavary.townmanager.Chatter;
 import me.sirsavary.townmanager.Main;
+import me.sirsavary.townmanager.TownMapRenderer;
 import me.sirsavary.townmanager.commands.town.TownClaim;
 import me.sirsavary.townmanager.commands.town.TownCreate;
 import me.sirsavary.townmanager.commands.town.TownInvite;
@@ -11,6 +12,7 @@ import me.sirsavary.townmanager.commands.town.TownRemove;
 import me.sirsavary.townmanager.commands.town.TownSettings;
 import me.sirsavary.townmanager.commands.town.TownTeleport;
 import me.sirsavary.townmanager.commands.town.TownUnclaim;
+import me.sirsavary.townmanager.commands.town.admin.TownAdminRemove;
 import me.sirsavary.townmanager.objects.Town;
 
 import org.bukkit.command.Command;
@@ -34,8 +36,7 @@ public class TownCommand implements CommandExecutor {
 		Player p = (Player) sender;
 		if (args.length == 0)
 		{
-			if (t == null)
-			{
+			if (t == null) {
 				sender.sendMessage(Chatter.Message("Currently, you are not part of a town!"));
 				sender.sendMessage(Chatter.Message("To create a town, type /town create"));
 				sender.sendMessage(Chatter.Message("To join a town, type /town join [TownName]"));
@@ -57,122 +58,197 @@ public class TownCommand implements CommandExecutor {
 				p.sendMessage(Chatter.Message("Type /town help for more info"));
 			}
 		}
-		else if (args.length == 1)
-		{
+		else if (args.length > 0) {
 			if (sender instanceof Player) {
-				if (args[0].equalsIgnoreCase("mayor"))
-				{
-					sender.sendMessage(Main.messageColor + "-----" + Main.tagColor + "[TownManager Help]" + Main.messageColor + "-----");
-					sender.sendMessage(Chatter.Message("/town mayor invite [Player] - Invite a player"));
-					sender.sendMessage(Chatter.Message("/town mayor claim - Claim a chunk"));
-					sender.sendMessage(Chatter.Message("/town mayor unclaim - Unclaim a chunk"));
-					sender.sendMessage(Chatter.Message("/town mayor settings - Edit town settings"));
-					sender.sendMessage(Chatter.Message("/town mayor plot - Top level plot command"));
-
+				if (args[0].equalsIgnoreCase("mayor")) {
+					MayorCommand(p, args);
+				} else if (args[0].equalsIgnoreCase("admin")) {
+					AdminCommand(p, args);
+				} else if (args[0].equalsIgnoreCase("plot")) {
+					PlotCommand(p, args);
+				} else {
+					CitizenCommand(p, args);
 				}
-				else if (args[0].equalsIgnoreCase("join")) sender.sendMessage(Chatter.TagMessage("You must specify a town to join!"));
-
-				else if (args[0].equalsIgnoreCase("tp") || args[0].equalsIgnoreCase("teleport") || args[0].equalsIgnoreCase("spawn") || args[0].equalsIgnoreCase("home")) new TownTeleport(p);
-				else if (args[0].equalsIgnoreCase("list")) ListTowns(p);
-				else if (args[0].equalsIgnoreCase("create")) {
-					try {
-						new TownCreate(sender, true, plugin);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+			} else {
+				if (args[0].equalsIgnoreCase("admin")) {
+					AdminCommand(p, args);
+				} else {
+					sender.sendMessage("You cannot use that command from console!");
 				}
-				else if (args[0].equalsIgnoreCase("leave")) {
-					try {
-						new TownLeave(sender, true, plugin, p);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-				else sender.sendMessage(Chatter.TagMessage("Not a valid command!"));
 			}
 		}
-		else if (args.length == 2)
-		{
-			if (args[0].equalsIgnoreCase("join"))
+		return true;
+	}
+	private void CitizenCommand(Player player, String[] args) {
+		if (args.length == 1) {
+			if (args[0].equalsIgnoreCase("join")) {
+				player.sendMessage(Chatter.TagMessage("You must specify a town to join!"));
+			} else if (args[0].equalsIgnoreCase("chunk")) {
+				player.sendMessage("Chunk is at cords (" + player.getLocation().getChunk().getX() + ","  + player.getLocation().getChunk().getZ() + ")");
+			} else if (args[0].equalsIgnoreCase("map")) {
+				TownMapRenderer.CreateMap(player);
+			} else if (args[0].equalsIgnoreCase("tp") || args[0].equalsIgnoreCase("teleport") || args[0].equalsIgnoreCase("spawn") || args[0].equalsIgnoreCase("home")) {
+				new TownTeleport(player);
+			} else if (args[0].equalsIgnoreCase("list")) {
+				ListTowns(player);
+			} else if (args[0].equalsIgnoreCase("create")) {
 				try {
-					new TownJoin(sender, true, plugin, args[1]);
+					new TownCreate(player, true, plugin);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-			else if (args[0].equalsIgnoreCase("mayor"))
-				if (args[1].equalsIgnoreCase("claim")) new TownClaim(p);
-				else if (args[1].equalsIgnoreCase("unclaim")) new TownUnclaim(p);
-				else if (args[1].equalsIgnoreCase("invite")) p.sendMessage(Chatter.TagMessage("Not enough args! Must specify player to invite!"));
-				else if (args[1].equalsIgnoreCase("kick")) p.sendMessage(Chatter.TagMessage("Not enough args! Must specify player to kick!"));
-				else if (args[1].equalsIgnoreCase("plot")) PlotHelp(p);
-				else if (args[1].equalsIgnoreCase("settings")) {
-					try {
-						new TownSettings(sender, true, plugin);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+			} else if (args[0].equalsIgnoreCase("leave")) {
+				try {
+					new TownLeave(player, true, plugin);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-				else if (args[1].equalsIgnoreCase("remove")) {
-					try {
-						new TownRemove(sender, true, plugin);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-				else sender.sendMessage(Chatter.TagMessage("Not a valid command!"));
-		}
-			else if (args.length == 3) {
-				if (args[0].equalsIgnoreCase("mayor")) {
-					if (args[1].equalsIgnoreCase("plot")) { //TODO
-						if (args[2].equalsIgnoreCase("new")) {
-
-						}
-						else if (args[2].equalsIgnoreCase("remove")) {
-
-						}
-						else if (args[2].equalsIgnoreCase("addowner")) {
-
-						}
-						else if (args[2].equalsIgnoreCase("delowner")) {
-
-						}
-						else if (args[2].equalsIgnoreCase("info")) {
-
-						} 
-						else PlotHelp(p);
-					}
-					else if (args[1].equalsIgnoreCase("invite"))
-						try {
-							new TownInvite(sender, true, plugin, args[2]);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					else sender.sendMessage(Chatter.TagMessage("Not a valid command!"));
-				}
-
+			} else {
+				player.sendMessage(Chatter.TagMessage("Not a valid command!"));
 			}
-		return true;
+		}
+		else if (args.length == 2)
+			if (args[0].equalsIgnoreCase("join")) {
+				String townName = null;
+				for (int x = 1; x < args.length; x++) {
+					townName = townName + args[x] + " ";
+				}
+				townName.trim();
+				try {
+					new TownJoin(player, true, plugin, townName);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
 	}
-
+	private void PlotCommand(Player player, String[] args) {
+		if (args.length <= 1) {
+			PlotHelp(player);
+		} else if (args.length == 2) {
+			if (args[1].equalsIgnoreCase("new")) {
+				;
+			} else if (args[1].equalsIgnoreCase("select")) {
+				player.sendMessage(Chatter.TagMessage("Please choose a plot to select!"));
+			} else {
+				player.sendMessage(Chatter.TagMessage("Not a valid command!"));
+			}
+		}
+		else if (args.length == 3) {
+			if (args[1].equalsIgnoreCase("remove")) {
+				;
+			} else if (args[1].equalsIgnoreCase("addmember")) {
+				;
+			} else if (args[1].equalsIgnoreCase("delmember")) {
+				;
+			} else if (args[1].equalsIgnoreCase("info")) {
+				;
+			} else {
+				player.sendMessage(Chatter.TagMessage("Not a valid command!"));
+			}
+		}
+	}
+	private void MayorCommand(Player player, String[] args) {
+		if (args.length == 1) {
+			MayorHelp(player);
+		}
+		else if (args.length == 2) {
+			if (args[0].equalsIgnoreCase("mayor"))
+				if (args[1].equalsIgnoreCase("claim")) {
+					new TownClaim(player);
+				} else if (args[1].equalsIgnoreCase("unclaim")) {
+					new TownUnclaim(player);
+				} else if (args[1].equalsIgnoreCase("invite")) {
+					player.sendMessage(Chatter.TagMessage("Not enough args! Must specify player to invite!"));
+				} else if (args[1].equalsIgnoreCase("kick")) {
+					player.sendMessage(Chatter.TagMessage("Not enough args! Must specify player to kick!"));
+				} else if (args[1].equalsIgnoreCase("plot")) {
+					PlotHelp(player);
+				} else if (args[1].equalsIgnoreCase("settings")) {
+					try {
+						new TownSettings(player, true, plugin);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				} else if (args[1].equalsIgnoreCase("remove")) {
+					try {
+						new TownRemove(player, true, plugin);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				} else {
+					player.sendMessage(Chatter.TagMessage("Not a valid command!"));
+				}
+		}
+		else if (args.length == 3)
+			if (args[1].equalsIgnoreCase("invite")) {
+				try {
+					new TownInvite(player, true, plugin, args[2]);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+	}
+	private void AdminCommand(Player player, String[] args) {
+		if (args.length == 1) {
+			AdminHelp(player);
+		}
+		else if (args.length == 2) {
+			if (args[1].equalsIgnoreCase("remove")) {
+				player.sendMessage(Chatter.TagMessage("Not enough args! Must specify town to remove!"));
+			} else {
+				player.sendMessage(Chatter.TagMessage("Not a valid command!"));
+			}
+		}
+		else if (args.length >= 3)
+			if (args[1].equalsIgnoreCase("remove")) {
+				String townName = null;
+				for (int x = 2; x < args.length; x++) {
+					townName = townName + args[x] + " ";
+				}
+				townName.trim();
+				try {
+					new TownAdminRemove(player, true, plugin, args[2]);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+	}
 	private void PlotHelp(Player p) {
-		p.sendMessage(Chatter.Message("/town mayor plot new [ID]- Makes a new plot"));
-		p.sendMessage(Chatter.Message("/town mayor plot remove [ID]"));
-		p.sendMessage(Chatter.Message("/town mayor plot addowner [ID] [OWNER] - Adds an owner to a plot"));
-		p.sendMessage(Chatter.Message("/town mayor plot delowner [ID] [OWNER] - Removes an owner from a plot"));
-		p.sendMessage(Chatter.Message("/town mayor plot info - Gets the info from the plot you are standing in"));
-		p.sendMessage(Chatter.Message("/town mayor plot info [ID] - Gets the info from a plot"));
+		p.sendMessage(Chatter.Message("/town plot new [ID]- Makes a new plot"));
+		p.sendMessage(Chatter.Message("/town plot remove [ID]"));
+		p.sendMessage(Chatter.Message("/town plot addowner [ID] [OWNER] - Adds an owner to a plot"));
+		p.sendMessage(Chatter.Message("/town plot delowner [ID] [OWNER] - Removes an owner from a plot"));
+		p.sendMessage(Chatter.Message("/town plot info - Gets the info from the plot you are standing in"));
+		p.sendMessage(Chatter.Message("/town plot info [ID] - Gets the info from a plot"));
+	}
+	private void AdminHelp(Player p) {
+		p.sendMessage(Chatter.Message("/town plot new [ID]- Makes a new plot"));
+		p.sendMessage(Chatter.Message("/town plot remove [ID]"));
+		p.sendMessage(Chatter.Message("/town plot addowner [ID] [OWNER] - Adds an owner to a plot"));
+		p.sendMessage(Chatter.Message("/town plot delowner [ID] [OWNER] - Removes an owner from a plot"));
+		p.sendMessage(Chatter.Message("/town plot info - Gets the info from the plot you are standing in"));
+		p.sendMessage(Chatter.Message("/town plot info [ID] - Gets the info from a plot"));
+	}
+	private void MayorHelp(Player p) {
+		p.sendMessage(Main.messageColor + "-----" + Main.tagColor + "[TownManager Help]" + Main.messageColor + "-----");
+		p.sendMessage(Chatter.Message("/town mayor invite [Player] - Invite a player"));
+		p.sendMessage(Chatter.Message("/town mayor claim - Claim a chunk"));
+		p.sendMessage(Chatter.Message("/town mayor unclaim - Unclaim a chunk"));
+		p.sendMessage(Chatter.Message("/town mayor settings - Edit town settings"));
 	}
 	private void ListTowns(Player player) {
 		String s = "";
-		if (Main.fileManager.getTowns() == null)
+		if (Main.fileManager.getTowns() == null) {
 			player.sendMessage(Chatter.TagMessage("There are no available towns!"));
-		else {
-			for (Town town : Main.fileManager.getTowns()) {
-				if (s.equalsIgnoreCase("")) s = town.getFormattedID();
-				else s = s + "," + town.getFormattedID();
-				player.sendMessage(Chatter.TagMessage("Available Towns:"));
-				player.sendMessage(Chatter.Message(s));
-			}
+		} else {
+			for (Town town : Main.fileManager.getTowns())
+				if (s.equalsIgnoreCase("")) {
+					s = town.getFormattedID();
+				} else {
+					s = s + ", " + town.getFormattedID();
+				}
+			player.sendMessage(Chatter.TagMessage("Available Towns:"));
+			player.sendMessage(Chatter.Message(s));
 		}
 	}
 }
